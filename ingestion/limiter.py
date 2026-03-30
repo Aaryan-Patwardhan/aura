12ecdfe27831d451707b8ce6b0d542e5c79d2d6d
Aaryan Patwardhan <patwardhanaaryan@gmail.com>
2026-03-31 01:20:18 +0530

    feat: implement Isolation Forest training pipeline

--- ingestion/limiter.py
+++ ingestion/limiter.py
@@ -1,7 +1,9 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-def api_key_func(req):
-    return req.headers.get("X-API-Key", get_remote_address(req))
+from fastapi import Request
+
+def api_key_func(request: Request):
+    return request.headers.get("X-API-Key", get_remote_address(request))
 
 limiter = Limiter(key_func=api_key_func, default_limits=["100/minute"])
